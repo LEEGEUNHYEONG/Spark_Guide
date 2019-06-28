21033d7b3dd6c744b2aec8d5fd15264872313281
Nanara <nanara1119@gmail.com>
2019-06-28 18:20:33 +0900

    1. Spark 실행 가능하도록 함

--- build.sbt
+++ build.sbt
@@ -2,4 +2,9 @@ name := "Spark_Guide"
 
 version := "0.1"
 
-scalaVersion := "2.13.0"
+scalaVersion := "2.12.7"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.3"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.3"
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,20 @@
+import org.apache.spark.sql.SparkSession
+
+
 object Hello extends App {
   println("Hello, World!")
+
+  override def main(args: Array[String]): Unit = {
+    var spark = SparkSession.builder()
+      .master("local[*]")
+      //.appName("Spark_Guide")
+      //.config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
+    val file_path = getClass.getResource("/data/2015-summary.json").getPath
+    val df = spark.read.json(file_path)
+    df.show()
+
+    //  todo  spark 로컬 shell에서 실행 하는 방법은 없는 건가 ??
+  }
 }
